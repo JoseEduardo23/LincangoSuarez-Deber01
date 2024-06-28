6fcbc6378276c9281eb86ed370c1627a6ffc47d3
Michelle Suarez <michelle.suarez@epn.edu.ec>
2024-06-28 17:02:25 -0500

    Swing terminado

---
+++ Calculadora/src/JAVACalculator.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class JAVACalculator {
+    private JTextField textField1;
+    private JPanel JAVACalculator;
+    private JButton clear, a3Button, a6Button, a7Button, a8Button, a4Button, a1Button, coma, a9Button, a5Button, a2Button, a0Button, suma, division, senButton, resta, raiz, cosButton, igual, multiplicacion, potencia, tanButton;
+}
